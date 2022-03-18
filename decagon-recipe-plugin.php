e1557518a03d993a399d211c21dde58916fee713
michael valentine <37175136+chionye@users.noreply.github.com>
2022-03-18 08:00:02 +0100

    Create the index and plugin page

---
+++ decagon-recipe-plugin.php
@@ -0,0 +1,30 @@
+<?php
+/**
+* @package DecagonRecipePlugin
+*/
+/*
+Plugin Name: Decagon Recipe Plugin
+Plugin URI: https://decagonhq.com/
+Description:A simple plugin that can be used to create a new recipe, view all available recipes, to edit a recipe and to delete a single or multiple recipes
+Version: 1.0.0
+Author: Valentine Michael
+Author URI: https://chionye.com/
+License: GPLv2 or later
+Text Domain: decagon-recipe-plugin
+*/
+
+/*
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+Copyright 2005-2015 Automattic, Inc.
+*/
+
