94a02a2d86a4e270ebc1946d51d5013ef7395d4c
michael valentine <37175136+chionye@users.noreply.github.com>
2022-03-18 08:05:47 +0100

    Add the check for unauthorized access

--- decagon-recipe-plugin.php
+++ decagon-recipe-plugin.php
@@ -28,3 +28,35 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 Copyright 2005-2015 Automattic, Inc.
 */
 
+defined( 'ABSPATH' ) or die();
+
+register_activation_hook( __FILE__, 'createTable');
+
+function createTable() {
+  global $wpdb;
+  $charsetCollate = $wpdb->get_charset_collate();
+  $tableName = $wpdb->prefix . 'recipe';
+  $sql = "CREATE TABLE IF NOT EXISTS `$tableName` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` text DEFAULT NULL,
+  `ingredients` text DEFAULT NULL,
+  `recipe` text DEFAULT NULL,
+  PRIMARY KEY(id)
+  ) $charsetCollate";
+
+  if ($wpdb->get_var("SHOW TABLES LIKE '$tableName'") != $tableName) {
+    require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+    dbDelta($sql);
+  }
+}
+
+add_action('admin_menu', 'addAdminMenu');
+
+function addAdminMenu() {
+    // menu items
+    add_menu_page('Recipes', 'Recipes', 'manage_options', 'Recipe', '', 'dashicons-community');
+    add_submenu_page( 'Recipe', 'All Recipes', 'All Recipes',
+        'manage_options', 'Recipe', 'getAllRecords');
+    add_submenu_page( 'Recipe', 'Edit Recipe', 'Edit Recipe',
+        'manage_options', 'Recipe', 'editRecipe');
+}
